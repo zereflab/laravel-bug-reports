cb53c7afebe5e23978ac43818bc8c30c96498335
S.Akash <36274335+Akash4686@users.noreply.github.com>
2026-06-13 01:01:36 +0500

    test

--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Zereflab\LaravelBugReports\Http\Controllers\SlackActionController;
-
-Route::post('slack/actions', SlackActionController::class)->name('bug-reports.slack.actions');
